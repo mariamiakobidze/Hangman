7a2fa907fd5ec0b94b6c48cdae94ac55650346e8
mariamiakobidze <mariamiakobidze4@gmail.com>
2025-10-20 19:17:40 +0400

    Lists and random

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -1,27 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-class Program
-{
-    static void Main()
-    {
-        string[] words = {
+﻿string[] words = {
             "computer", "apple", "classrom", "program",
             "sunshine", "mountain", "river", "school",
             "internet", "puzzle"
         };
 
-        Random random = new Random();
-        string word = words[random.Next(words.Length)];
+Random random = new Random();
+string word = words[random.Next(words.Length)];
 
-        char[] hiddenWord = new char[word.Length];
-        for (int i = 0; i < word.Length; i++)
-            hiddenWord[i] = '_';
+char[] hiddenWord = new char[word.Length];
+for (int i = 0; i < word.Length; i++)
+    hiddenWord[i] = '_';
 
-        List<char> guessedLetters = new List<char>();
-        int triesLeft = 6;
-        bool wordGuessed = false;
+List<char> guessedLetters = new List<char>();
+int triesLeft = 6;
+bool wordGuessed = false;
 
-        Console.WriteLine("🎯 Welcome to Hangman!");
-        Console.WriteLine("----------------------");
-    }
-}
-
+Console.WriteLine("🎯 Welcome to Hangman!");
+Console.WriteLine("----------------------");
