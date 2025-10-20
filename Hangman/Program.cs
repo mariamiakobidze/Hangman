c05ed67289e008dc6ef978d9612e31f3ea8721e0
mariamiakobidze <mariamiakobidze4@gmail.com>
2025-10-20 12:36:06 +0400

    list and ramdom

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -1,6 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main()
+    {
+        string[] words = {
+            "computer", "apple", "classrom", "program",
+            "sunshine", "mountain", "river", "school",
+            "internet", "puzzle"
+        };
 
-Console.WriteLine();
+        Random random = new Random();
+        string word = words[random.Next(words.Length)];
 
-Console.WriteLine("smth");
+        char[] hiddenWord = new char[word.Length];
+        for (int i = 0; i < word.Length; i++)
+            hiddenWord[i] = '_';
+
+        List<char> guessedLetters = new List<char>();
+        int triesLeft = 6;
+        bool wordGuessed = false;
+
+        Console.WriteLine("🎯 Welcome to Hangman!");
+        Console.WriteLine("----------------------");
+    }
+}
+
